b101ebbc7a5988a533212fd86a7f0f819ab34388
roboman2444 <roboman2444@gmail.com>
2014-01-09 13:03:02 -0500

    this might actually load an obj... maybe not

--- modelmanager.c
+++ modelmanager.c
@@ -80,20 +80,17 @@ int loadModelOBJ(model_t * m, char * filename){//todo flags
 	unsigned int readnorm = 0;
 	unsigned int readtc = 0;
 	unsigned int readface = 0;
-	unsigned int readinterleaved = 0;
 	while(fgets(line, 300, f)){
 		testline = line;
 		while(*testline == ' ' || *testline == '\t') testline+=sizeof(char); //should be 1 //take off early white spaces
 		if(testline > 200 + line) continue;
 		if(!strncmp(testline, "v ", 2)){
 			if(readvert >= vertcount) break; //todo debug
-//			sscanf(line," v %f %f %f",&vertbuffer[readvert++],&vertbuffer[readvert++],&vertbuffer[readvert++]);
 			sscanf(line," v %f %f %f",&vertbuffer[(readvert*3)],&vertbuffer[(readvert*3)+1],&vertbuffer[(readvert*3)+2]);
 			readvert++;
 		}
 		else if(!strncmp(testline, "vt", 2)){
 			if(readtc >= tccount) break; //todo debug
-//			sscanf(line," vt %f %f",&tcbuffer[readtc++],&tcbuffer[readtc++]);
 			sscanf(line," vt %f %f",&tcbuffer[(readtc*2)],&tcbuffer[(readtc*2)+1]);
 			readtc++;
 		}
@@ -104,7 +101,6 @@ int loadModelOBJ(model_t * m, char * filename){//todo flags
 		}
 		else if(!strncmp(testline, "f ", 2)){
 			if(readface >=facecount) break;
-			//sscanf(line," vp %d %d %d", &facebuffer[readface++], &facebuffer[readface++], &facebuffer[readface++]);
 			if(sscanf(line," f %d/%d/%d %d/%d/%d %d/%d/%d",
 				&facebuffer[0], &facebuffer[1], &facebuffer[2],
 				&facebuffer[3], &facebuffer[4], &facebuffer[5],
@@ -119,28 +115,37 @@ int loadModelOBJ(model_t * m, char * filename){//todo flags
 				&facebuffer[6], &facebuffer[7], &facebuffer[8]
 			) == 6);
 			else return 0; //todo debug... most likely case is that it has a quad
-			//todo skip reading into a buffer and parse the iqm out into usable data here
-			//needed for that -2 relative crap
+
 			int n;
 			for(n = 0; n < 3; n++){ //two or more, use a for
-				int vindice;
-				int tcindice;
-				int normindice;
+				int vindice = 0;
+				int tcindice = 0;
+				int normindice = 0;
 				if(facebuffer[(n*3)+0] > 0) vindice = facebuffer[(n*3)+0];
 				else if(facebuffer[(n*3)+0] < 0) vindice = readvert+facebuffer[(n*3)+0];
 				else return 0; //0, todo debug
 				if(facebuffer[(n*3)+0] >= 0) tcindice = tcbuffer[(n*3)+1];
 				else if(facebuffer[(n*3)+0] < 0) tcindice = readtc+facebuffer[(n*3)+1];
-//				else return 0; //0, todo debug
+
 				if(facebuffer[(n*3)+1] >= 0) normindice = facebuffer[(n*3)+2];
 				else if(facebuffer[(n*3)+2] < 0) normindice = readnorm+facebuffer[(n*3)+2];
-//				else return 0; //0, todo debug
 
 				//may not be doing this properly...
 				indicebuffer[readface+n] = vindice;
-				
-
-				//if(interleavedbuffer[
+				interleavedbuffer[(vindice*8)+0] = vertbuffer[(vindice*3)+0];
+				interleavedbuffer[(vindice*8)+1] = vertbuffer[(vindice*3)+1];
+				interleavedbuffer[(vindice*8)+2] = vertbuffer[(vindice*3)+2];
+				if(normindice){
+					//todo maybe check if there is already something there...
+					interleavedbuffer[(vindice*8)+3] = vertbuffer[(normindice*3)+0];
+					interleavedbuffer[(vindice*8)+4] = vertbuffer[(normindice*3)+1];
+					interleavedbuffer[(vindice*8)+5] = vertbuffer[(normindice*3)+2];
+				}
+				if(normindice){
+					//todo check if there is maybe something there
+					interleavedbuffer[(vindice*8)+6] = tcbuffer[(tcindice*2)+0];
+					interleavedbuffer[(vindice*8)+7] = tcbuffer[(tcindice*2)+1];
+				}
 				//todo, gotta think about this for a while
 			}
 			readface++;
